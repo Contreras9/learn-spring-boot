c54fea4528469dfd84a31c25a797ae9cb2f0a504
Hamzah Contreras <h.contreras9@outlook.com>
2023-07-21 20:28:02 -0500

    Build a simple REST API using SpringBoot

---
+++ src/main/java/com/in28minutes/springboot/learnspringboot/Course.java
@@ -0,0 +1,49 @@
+package com.in28minutes.springboot.learnspringboot;
+
+import java.util.Objects;
+
+public class Course {
+    private long id;
+    private String name;
+    private String author;
+
+    public Course(long id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && Objects.equals(name, course.name) && Objects.equals(author, course.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, author);
+    }
+}
